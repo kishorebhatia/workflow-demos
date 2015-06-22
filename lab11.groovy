701481a1caddd4c186406ea437cb648f3ad84bc3
Kishore Bhatia <bhatia.kishore@gmail.com>
2015-06-22 15:15:19 -0400

    Create lab11.groovy

---
+++ lab11.groovy
@@ -0,0 +1,17 @@
+node('docker') { // COMPILE AND JUNIT
+    def src = 'https://github.com/harniman/spring-petclinic.git'
+    // def src = '/Users/nharniman/git/harniman/spring-petclinic'
+    git url: src
+
+    ensureMaven()
+    //sh 'mvn -o clean package'
+     sh 'mvn clean package'
+}
+
+
+/**
+ * Deploy Maven on the slave if needed and add it to the path
+ */
+def ensureMaven() {
+    env.PATH = "${tool 'Maven 3.x'}/bin:${env.PATH}"
+}
