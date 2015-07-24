eae1c8118dcb79431d45943aef92c2d973bc23e0
Kishore Bhatia <bhatia.kishore@gmail.com>
2015-07-24 15:14:36 -0400

    Update lab-1.groovy

--- lab-1.groovy
+++ lab-1.groovy
@@ -2,6 +2,8 @@ node('linux') { // COMPILE AND JUNIT
     def src = 'https://github.com/harniman/spring-petclinic.git'
     // def src = '/Users/nharniman/git/harniman/spring-petclinic'
     git url: src
+    
+    sh 'sleep 30'
 
     ensureMaven()
     //sh 'mvn -o clean package'
