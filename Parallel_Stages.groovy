2b22dcb4b076190b2aaf5716772176f4fd40dddd
Kishore Bhatia <bhatia.kishore@gmail.com>
2016-02-29 10:20:41 -0500

    Update Parallel_Stages.groovy

--- Parallel_Stages.groovy
+++ Parallel_Stages.groovy
@@ -1,13 +1,13 @@
 def tasks = [:]
 
 tasks["vagrant-1"] = {
-  node('vagrant-slave') {  
+  node('docker') {  
     stage concurrency: 1, name: 'vagrant-slave-1'
     sh 'echo $NODE_NAME' 
   }
 }
 tasks["vagrant-2"] = {
-  node('vagrant-slave-2') { 
+  node('docker-cloud') { 
     stage concurrency: 2, name: 'vagrant-slave-2'
     sh 'echo $NODE_NAME' 
   }
